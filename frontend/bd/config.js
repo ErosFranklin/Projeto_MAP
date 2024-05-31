c147b934c481ec4b77fa3ba6130f74f2ca1bba3d
JoseGuilherme0 <jose.morato@aluno.uepb.edu.br>
2024-05-31 20:04:04 -0300

    backend+bd_localhost

---
+++ frontend/bd/config.js
@@ -0,0 +1,39 @@
+//O Banco de dados se encontra no seguinte seguimento:C:\ProgramData\MySQL\MySQL Server 8.0\Data\sistemadecadastro
+
+//Para cria-lo foi usado o MySQL Workbench, por ser mais dinâmico e fácil de se trabalhar;
+/*/Foram dados os seguintes comandos:
+
+1- Para criação do Banco de Dados:
+
+CREATE SCHEMA `sistemadecadastro` DEFAULT CHARACTER SET utf8mb4 ;
+
+2-Para criação da tabela principal que foi atribuida o nome de usuários:
+
+CREATE TABLE `sistemadecadastro`.`usuarios` (
+  `idusuario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `senha` VARCHAR(200) NOT NULL,
+  `formacao` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusuarios`));
+
+3-
+
+
+
+
+/*/
+//sujeito a alteração!!!
+//Iniciando o Banco de Dados para nossa aplicação:
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "16140112",
+  database: "sistemadecadastro",
+});
+
+//Exportamos para que nosso arquivo backend.js consiga acessa-lo:
+module.exports = pool.promise();
