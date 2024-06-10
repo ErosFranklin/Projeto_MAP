4484aba55cba3d52e73951c0b0daf8c971624827
Kaio Vinícius Cordeiro Batista <kaiovbatista19@gmail.com>
2024-06-10 18:33:48 -0300

    Update scriptdobancoSMASS.sql, add insercoespredefinidasnobancoSMAAS.sql and add updatesnobancoSMAAS.sql
    Atualização do script e separação das inserções e updates no banco em arquivos únicos.

--- frontend/bd/scriptdobancoSMAAS.sql
+++ frontend/bd/scriptdobancoSMAAS.sql
@@ -1,4 +1,4 @@
-/*--------- TABELAS E INSERÇÕES ----------*/
+/*--------- TABELAS ----------*/
 /*CRIANDO BANCO*/
 create schema SMAAS;
 
@@ -16,44 +16,6 @@ CREATE TABLE agente (
   PRIMARY KEY (id_agente)
 );
 
-alter table agente modify column email VARCHAR(200) NOT NULL UNIQUE;
-alter table agente modify column cpf VARCHAR(15) NOT NULL UNIQUE;
-alter table agente modify column rg VARCHAR(12) NOT NULL UNIQUE;
-
-/*CRIANDO AGENTES*/
-insert into agente(nome, email, senha, formacao, data_contratacao, cpf, rg, telefone) values (
-    'Luiz Amilton',
-    'luizamilton_f1@gmail.com',
-    'amilton123',
-    'Técnico em Enfermagem',
-    '2022-07-01',
-    '43035670120',
-    '3202304',
-    '83996512424'
-);
-
-insert into agente(nome, email, senha, formacao, data_contratacao, cpf, rg, telefone) values (
-    'Max Venício',
-    'maxvenicio_f1@gmail.com',
-    'venicio456',
-    'Enfermeiro',
-    '2023-04-26', 
-    '33221140155',
-    '5100222',
-    '83998700722'
-);
-
-insert into agente(nome, email, senha, formacao, data_contratacao, cpf, rg, telefone)  values(
-    'Henrique Alfredo',
-    'henrique167@gmail.com',
-    'henr0072',
-    'Enfermeiro',
-    '2015-06-12',
-    '13212418334',
-    '4502338',
-    '83986654399'
-);
-
 /*CRIANDO POLO USF*/
 create table polo_usf (
     id_polo int unsigned not null unique auto_increment primary key,
@@ -62,23 +24,6 @@ create table polo_usf (
     numero int not null,
     bairro varchar(50) not null,
     cidade varchar(50) not null
-);
-
-/*CRIANDO POLOS*/
-insert into polo_usf(quant_agentes, rua, numero, bairro, cidade) values (
-    13,
-    'Rua das Chagas',
-    157,
-    'Santo Expedito',
-    'Santa Luzia'
-);
-
-insert into polo_usf(quant_agentes, rua, numero, bairro, cidade) values (
-    10,
-    'Rua Padre Anchieta',
-    205,
-    'Santo Antonio',
-    'Santa Luzia'
 );
 
 /*CRIANDO TABELA PARA UNIR OS AGENTES COM OS POLOS*/
@@ -90,19 +35,6 @@ create table poloAgente (
     foreign key (id_polo) references polo_usf(id_polo)
 );
 
-/*UNINDO OS AGENTES AOS SEUS RESPECTIVOS POLOS*/
-insert into poloAgente(id_agente, id_polo) values (
-    1,1
-);
-
-insert into poloAgente(id_agente, id_polo) values (
-    2,2
-);
-
-insert into poloAgente(id_agente, id_polo) values (
-    3,1
-);
-
 /*CRIANDO TABELA FAMILIA*/
 create table familia (
 	id_familia int unsigned not null unique auto_increment primary key,
@@ -112,29 +44,6 @@ create table familia (
     bairro varchar(50) not null,
     numero int not null
 );
-
-/*INSERINDO FAMILIAS COM OS SEUS RESPONSÁVEIS*/
-insert into familia(responsavel, numero_dependentes, rua, bairro, numero) values (
-	'Vinicius Paixão de Oliveira',
-    2,
-    'Rua Augusto dos Anjos',
-    'Santo Antonio',
-    70
-);
-
-insert into familia(responsavel, numero_dependentes, rua, bairro, numero) values (
-	'Gabriel Oliveira Ferreira',
-    5,
-    'Rua dos Desesperados',
-    'Saudades',
-    100
-);
-
-/*Alterando número de dependentes da família/responsável com id 2*/
-update familia set numero_dependentes = 2 where id_familia = 2;
-
-/*Atlerando bairro da família com id = 2 para o Santo Expedito, um bairro com polo*/
-update familia set bairro = 'Santo Expedito' where id_familia = 2;
 
 /*CRIANDO A TABELA PACIENTE*/
 create table paciente(
@@ -150,79 +59,6 @@ create table paciente(
 	alergias varchar(200)
 );
 
-/*INSERINDO PACIENTES NO BANCO*/
-insert into paciente(nome, altura, peso, cpf, rg, telefone, email, doencas_cronicas, alergias) values (
-	'Erasmo Franklin dos Santos',
-    1.98,
-    85,
-    '10195609908',
-    '5432100',
-    '87996500010',
-	'santos_franklin201@gmail.com',
-    'Diabetes',
-    'Poeira e pelo de cachorro'
-);
-
-insert into paciente(nome, altura, peso, cpf, rg, telefone, email, doencas_cronicas, alergias) values (
-	'Maria Augusta Vieira Rodrigues',
-    1.55,
-    58,
-    '30322259087',
-    '23801012',
-    '83996907171',
-	'rodriguesmav@gmail.com',
-    'Hipertensão',
-    'Poeira'
-);
-
-insert into paciente(nome, altura, peso, cpf, rg, telefone, email, doencas_cronicas, alergias) values (
-	'Helena Tavares Silva',
-    1.51,
-    49,
-    '44012090011',
-    '3456708',
-    '83998999921',
-	'helena57silva@gmail.com',
-    '',
-    ''
-);
-
-insert into paciente(nome, altura, peso, cpf, rg, telefone, email, doencas_cronicas, alergias) values (
-	'José Maria Menezes Soares',
-    1.71,
-    77.2,
-    '40101295077',
-    '4901033',
-    '87998123340',
-	'menezesmariajose@gmail.com',
-    'Tireoideite',
-    'Alho'
-);
-
-insert into paciente(nome, altura, peso, cpf, rg, telefone, email, doencas_cronicas, alergias) values (
-	'Vinicius Paixão de Oliveira',
-    1.82,
-    65.5,
-    '43020012099',
-    '5555601',
-    '84996550014',
-	'paixao1212@gmail.com',
-    '',
-    'Ovo, cenoura e amendoim'
-);
-
-insert into paciente(nome, altura, peso, cpf, rg, telefone, email, doencas_cronicas, alergias) values (
-	'Gabriel Oliveira Ferreira',
-    1.70,
-    58,
-    '33312094507',
-    '1200333',
-    '87996999296',
-	'gabriel.ferreira@gmail.com',
-    'AVC',
-    ''
-);
-
 /*Unindo paciente com suas respectivas famílias na tabela pacienteFamilia*/
 create table pacienteFamilia (
 	id_pacienteFamilia int unsigned not null unique auto_increment primary key,
@@ -230,30 +66,6 @@ create table pacienteFamilia (
     id_familia int unsigned not null,
     foreign key (id_paciente) references paciente(id_paciente),
     foreign key (id_familia) references familia(id_familia)
-);
-
-insert into pacienteFamilia(id_paciente, id_familia) values (
-	1,2
-);
-
-insert into pacienteFamilia(id_paciente, id_familia) values (
-	4,2
-);
-
-insert into pacienteFamilia(id_paciente, id_familia) values (
-	6,2
-);
-
-insert into pacienteFamilia(id_paciente, id_familia) values (
-	2,1
-);
-
-insert into pacienteFamilia(id_paciente, id_familia) values (
-	3,1
-);
-
-insert into pacienteFamilia(id_paciente, id_familia) values (
-	5,1
 );
 
 /*Criando a tabela que guardará as informações das visitas*/
@@ -265,6 +77,7 @@ create table visita(
     data_da_visita date not null,
     foreign key (id_familia) references familia(id_familia),
     foreign key (id_agente) references agente(id_agente)
+<<<<<<< Updated upstream:frontend/bd/scriptdobancoSMAAS.sql
 );
 
 /*Marcando visitas nas tabelas*/
@@ -302,3 +115,6 @@ alter table visita add column status_da_visita boolean not null;
 update visita set status_da_visita = false where id_visita = 1;
 update visita set status_da_visita = true where id_visita = 2;
 update visita set status_da_visita = true where id_visita = 3;
+=======
+);
+>>>>>>> Stashed changes:SMAAS/bd/scriptdobancoSMAAS.sql
